a8caa2a71d4b2284167ec6699e7a7950b960003d
mansour365 <54915792+mansour365@users.noreply.github.com>
2023-12-08 16:04:02 -0500

    Removed unnecessary code + fixed typo

--- myscript.js
+++ myscript.js
@@ -1,19 +1,16 @@
-//When user scrolls down 80px from top -> resize navbar padding and logo font size
+//When user scrolls down 1px from top -> add navbar shadow
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop >1 || document.documentElement.scrollTop > 1) {
-        //document.getElementById("navbar").style.padding = "6px 0px"; //padding when scroll
-        //document.getElementById("logo").style.fontSize = "25px"; //logo size when scroll
         document.getElementById("navbar").style.boxShadow = "0 0 6px 3px rgba(0, 0, 0, 0.3)";//Add shadow
     } 
     else {
-        //document.getElementById("navbar").style.padding = "6px 0px"; //padding when didn't scroll
-        //document.getElementById("logo").style.fontSize = "25px"; //logo size when didn't scroll
         document.getElementById("navbar").style.boxShadow = "none";//Remove shadow
     }
 }
+
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function toggleVerticalMenu() {
@@ -39,6 +36,7 @@ function hideVerticalMenu() {
 
 
 
+
 /*For stagered icons*/
 
 /*Create intersection observer*/
@@ -55,8 +53,10 @@ const observer = new IntersectionObserver((entries)=>{     //class that takes ca
 });
 
 
- const hiddenElements = document.querySelectorAll('.hidden');
- hiddenElements.forEach((el) => observer.observe(el));  //loop over all hidden elements, tell observer to observe each one of them
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));  //loop over all hidden elements, tell observer to observe each one of them
 
 
 
@@ -66,20 +66,20 @@ const observer = new IntersectionObserver((entries)=>{     //class that takes ca
 
 /*Different method used for quote animations*/
  function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+        var reveals = document.querySelectorAll(".reveal");
 
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 75; /*how many pixels under the element under it appears*/
+        for (var i = 0; i < reveals.length; i++) {
+            var windowHeight = window.innerHeight;
+            var elementTop = reveals[i].getBoundingClientRect().top;
+            var elementVisible = 75; /*how many pixels under the element under it appears*/
 
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } 
-      else {
-        reveals[i].classList.remove("active");
-      }
-    }
+            if (elementTop < windowHeight - elementVisible) {
+                reveals[i].classList.add("active");
+            } 
+            else {
+                reveals[i].classList.remove("active");
+            }
+        }
   }
 
 window.addEventListener("scroll", reveal);
